89a556d1bb9383b33ffa67f6e0aef338fa201dc6
Diogo-Barauna <118781846+Diogo-Barauna@users.noreply.github.com>
2022-12-14 18:21:09 -0300

    Add files via upload

--- Jokenpo.py
+++ Jokenpo.py
@@ -10,30 +10,34 @@ while res != 'N':
     [ 2 ] - Papel 
     [ 3 ] - Tesoura''')
     escolha = int(input('    Escolha: '))
+    #Atribuindo a escolha a uma jogada
     if escolha == 1:
         jogada = 'Pedra'
     elif escolha == 2:
         jogada = 'Papel'
     elif escolha == 3:
         jogada = 'Tesoura'
-
+    #Jogada do pc (randomizador)
     pc = ['Pedra', 'Papel', 'Tesoura']
     jogadapc = choice(pc)
     sleep(0.5)
     print('\nSeu oponente escolheu {} e você {}\n'.format(jogadapc, jogada))
     sleep(0.5)
+    #Condição de vitória
     if escolha == 1 and jogadapc == 'Tesoura' or escolha == 2 and jogadapc == 'Pedra' or escolha == 3 and jogadapc == 'Papel':
-        print('                {}{}{}\n'.format('\033[32m', 'VOCÊ VENCEU!!','\033[m'))
+        print('               {}{}{}\n'.format('\033[32m', 'VOCÊ VENCEU!!','\033[m'))
         vit = vit + 1
+    #Condição de derrota
     elif escolha == 1 and jogadapc == 'Papel' or escolha == 2 and jogadapc == 'Tesoura' or escolha == 3 and jogadapc == 'Pedra':
         print ('\033[31m               VOCÊ PERDEU :C \033[m \n')
         vitpc = vitpc + 1
     else:
         print('\033[34m                 EMPATE!\033[m \n')
-
+    #Contador de vitórias e derrotas
     print('Você está com {}{}{} vitórias'.format('\033[33m',vit,'\033[m'))
     print('O computador está com {}{}{} vitórias\n'.format('\033[33m',vitpc,'\033[m'))
-    res = str(input('Jogar novamente? S/N: ')).upper()
+    #Teste resposta sim ou não
+    res = str(input('Jogar novamente? S/N:\n ')).upper()
     if res == 'N':
         print('Obrigado por jogar, até a próxima!')
         break
